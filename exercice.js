0d6ccdb5ea1318a888b25bfbd6e6d73e34b79f24
Ai-ENGINEER-s <barrysanoussa19@gmail.com>
2024-09-13 15:34:47 +0100

    javaScript project

--- exercice.js
+++ exercice.js
@@ -9,47 +9,54 @@ let stock = [
     { name: "Fridge", category: "Electronics", quantity: 5 }
 ]
 
-function totalItemByCategory(category){
 
-    return stock
-    .filter(item => item.category === category )
-    .reduce((total , item) => total + item.quantity , 0)
+
+
+console.log("Crée une fonction totalItemsByCategory qui prend la catégorie en entrée (ex : Electronics) et retourne le nombre total d’articles pour cette catégorie. \n ")
+
+function totalItemsByCategory(category){
+
+    return stock.filter(
+    item => item.category === category
+    )
+    .reduce(
+      ( total , item  ) => total += item.quantity , 0
+    )
+}
+
+
+console.log(totalItemsByCategory('Electronics'))
+
+
+console.log("Crée une fonction restockItem qui prend le nom d’un produit et un nombre à ajouter, puis met à jour la quantité de ce produit dans le stock.\n")
+
+function restockItem(itemName , quantityToAdd){
+
+    let item = stock.find(item => item.name=== itemName)
+    if (item){
+   
+      item.quantity += quantityToAdd
+
+    }
 
 }
 
-console.log(totalItemByCategory("Clothing"))
+
+restockItem("Phone", 89)
+
+console.log((stock[2].quantity))
 
 
+console.log("Crée une fonction findLowStock qui retourne un tableau contenant tous les produits dont la quantité est inférieure à 20.")
 
 
-console.log("function pour ajouter un produit")
+function  findLowStock(lowStockItem){
+
+  return stock.filter(
+    item => item.quantity <=lowStockItem
+  )
 
 
-function restockItem(itemName,quantityToAdd){ 
-
-let item = stock.find(item => item.name === itemName); 
-
-if (item){
-    item.quantity += quantityToAdd
 }
 
-}
-
-restockItem("Laptop" , 1)
-
-console.log(stock[0].quantity)
-
-
-console.log("Function pour les articles  a stock faible ")
-
-function findLowStock(quantityOfStock){
-
-let lowStockItem = stock.filter(item => item.quantity <= quantityOfStock
-
-    
-)
-
-return lowStockItem
-
-}
 console.log(findLowStock(100))
