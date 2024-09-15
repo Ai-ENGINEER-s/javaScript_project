06c818e0fca93279f11cc8976f3707902e197686
Ai-ENGINEER-s <barrysanoussa19@gmail.com>
2024-09-15 20:55:20 +0100

    timer application

--- exercicesDom/execice2.js
+++ exercicesDom/execice2.js
@@ -36,8 +36,33 @@ function calculateAverage(scores){
         catch(error){
 
 
-            
+
         }
     })
 
 }
+
+
+
+console.log("function flechées en javaScript ")
+
+
+
+
+const add = (a ,b )=>{return a+b }
+
+
+function Timer(){
+    this.seconds = 0;
+    setInterval(()=>{
+        this.seconds++;
+        console.log(this.seconds)
+    },10)
+}
+
+const timer = new Timer()
+const squareNumbers =  numbers => {
+    squareNumbers**2
+}
+
+console.log(squareNumbers([1,2,3,4,]))
