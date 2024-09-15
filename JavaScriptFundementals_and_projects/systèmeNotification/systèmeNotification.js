6ce7ca64e9b650bf5661fcfc8a504d4b7c71aa94
Ai-ENGINEER-s <barrysanoussa19@gmail.com>
2024-09-15 18:44:26 +0100

    java project

---
+++ JavaScriptFundementals_and_projects/systèmeNotification/systèmeNotification.js
@@ -0,0 +1,16 @@
+function showNotification(message){
+
+const  time = 5*setTimeout(1000)
+
+
+console.log("ce message s'affichera dans 5 secondes",message+ time)
+
+
+}
+
+
+showNotification("BARRY SANOUSSA LOVES GOD")
+
+
+
+
