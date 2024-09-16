d25a8e7d16fd4d8ff7733fc7ae0a32e0863d5520
Ai-ENGINEER-s <barrysanoussa19@gmail.com>
2024-09-16 14:46:57 +0100

    functions in javaScript

--- exercicesDom/functions.js
+++ exercicesDom/functions.js
@@ -1,15 +1,17 @@
-"what is a function ? A function can defined as piece of code reusable and reproductible to perform a specific task "
+console.log("JavaScript provides a lot of method for data manipulation . these methodes are used in array a a lot so if you wanna be an expert in the web you have to master it . \n")
 
 
-function sayHi(name) { 
+// Frist methode is map()
 
-console.log('Hi ' + name)
-
-
-}
-
-
-sayHi('BARRY SANOUSSA')
+console.log("Map method is used to create a new array by applying a function to each element of an  existed array ")
 
 
 
+// map method take three parameters , element , index ,existedArray 
+
+
+
+// const newArray = array.map((element , index , array)=> {return some value for new array })
+
+
+# 
